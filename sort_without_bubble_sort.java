e2e80042480c5e49496968485217fb0c93423a8e
Rashmi1664 <rashmikiran166@gmail.com>
2020-07-15 09:04:58 -0700

    Add files via upload

---
+++ sort_without_bubble_sort.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Main
+
+{
+	
+public static void main(String[] args) 
+	
+{
+		
+int n,i;
+		
+		
+Scanner sc=new Scanner(System.in);
+		
+n=sc.nextInt();
+		
+		
+int a[]=new int[n];
+		
+		
+for(i=0;i<n;i++)
+		
+{
+		    
+a[i]=sc.nextInt();
+		
+}
+		
+Arrays.sort(a);
+		
+		
+for(i=0;i<n;i++)
+		
+{
+		    
+System.out.print(a[i]+" ");
+		
+}
+	
+}
+
+}
