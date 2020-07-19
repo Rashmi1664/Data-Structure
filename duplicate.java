7b712e0314b8c964379b639214464782b969526e
Rashmi1664 <rashmikiran166@gmail.com>
2020-07-18 21:09:42 -0700

    Add files via upload

---
+++ duplicate.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class Main
+
+{
+	
+public static void main(String[] args) 
+	
+{
+	   
+ int i,j,n;
+		
+Scanner sc=new Scanner(System.in);
+	   
+ n=sc.nextInt();
+	   
+	    
+	    
+int a[]=new int[n];
+	   
+ for(i=0;i<n;i++)
+	    
+{
+	      
+  a[i]=sc.nextInt();
+	   
+ }
+	    
+	    
+for(i=0;i<n;i++)
+	    
+{
+	        
+for(j=i+1;j<n;j++)
+	       
+ {
+	          
+  if(a[i]==a[j])
+	        
+    {
+	              
+  System.out.print( a[i]+" ");
+	          
+  }
+	       
+ }
+	   
+ }
+	
+}
+
+}
+
