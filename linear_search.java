23948301b8f225955afd7620e47ddff1d788c035
Rashmi1664 <rashmikiran166@gmail.com>
2020-07-22 21:36:31 -0700

    Update linear_search.java

--- linear_search.java
+++ linear_search.java
@@ -1,77 +1,36 @@
 import java.util.*;
 
-
-
-public class Main
-
+public class ss
 {
-   
- public static int linear(int a[],int num)
-	
-{
-	    
+ public static int linear(int a[],int num)	
+{    
 int i;
-	    
-for(i=0;i<a.length;i++)
-		
-{
-		    
-if(a[i]==num)
-		    
-{
-		        
-return i;
-		   
+for(i=0;i<a.length;i++)		
+{    
+if(a[i]==num)		    
+{	        
+return i;		   
+}		
+}	 
+return -1;	
 }
-		   
-		
-}
-		 
-return -1;
-	
-}
-
-	
-	
-public static void main(String[] args)
-	
-{
-		
+public static void main(String[] args)	
+{	
 int n,num,i;
-		
-		
 Scanner sc=new Scanner(System.in);
-		
 n=sc.nextInt();
-		
 System.out.println("Enter the number you want to search");
-		
 num=sc.nextInt();
-		
-		
 int a[]=new int[n];
-		
-for(i=0;i<n;i++)
-		
+for(i=0;i<n;i++)		
 {
-		   
- a[i]=sc.nextInt();
-		
+ a[i]=sc.nextInt();		
 }
-		
-    	
 int k=linear(a,num);
-	
-	   
  if(k == -1)
-		   
- System.out.print("Number not found");
-		
+ System.out.print("Number not found");		
 else
-		   
  System.out.print("Number found at "+ i);
-
 }
-
 }
 
