b49fb7a6579eab78e1cb792089158ce65610a826
Rashmi1664 <rashmikiran166@gmail.com>
2020-07-22 21:38:29 -0700

    Update sort_without_bubble_sort.java

--- sort_without_bubble_sort.java
+++ sort_without_bubble_sort.java
@@ -1,43 +1,21 @@
 import java.util.*;
 
-public class Main
-
+public class main
 {
-	
-public static void main(String[] args) 
-	
-{
-		
+public static void main(String[] args) 	
+{	
 int n,i;
-		
-		
 Scanner sc=new Scanner(System.in);
-		
 n=sc.nextInt();
-		
-		
 int a[]=new int[n];
-		
-		
-for(i=0;i<n;i++)
-		
-{
-		    
-a[i]=sc.nextInt();
-		
+for(i=0;i<n;i++)		
+{  
+a[i]=sc.nextInt();		
 }
-		
 Arrays.sort(a);
-		
-		
-for(i=0;i<n;i++)
-		
+for(i=0;i<n;i++)		
 {
-		    
-System.out.print(a[i]+" ");
-		
+System.out.print(a[i]+" ");		
+}	
 }
-	
-}
-
 }
